77107d911b635a64d656a29bc3fc7fd4caa92ec6
jeanssresende <jean.resende@professor.fpp.edu.br>
2026-08-08 08:37:04 -0300

    Melhora tela inicial e responsividade da interface principal

--- ui.R
+++ ui.R
@@ -3,49 +3,55 @@ library(shinyjs)
 library(shinyFiles)
 library(shinyWidgets)
 
-# Carregando UI
 source("ui_qualidade.R")
 source("ui_trimagem.R")
 
-# FRONT
+tema_intra <- bs_theme(
+  version = 5,
+  bootswatch = "flatly",
+  base_font = font_google("Lexend Deca")
+)
 
 ui <- fluidPage(
   
-  theme = bs_theme(version = 5,
-                   bootswatch = "flatly", 
-                   base_font = font_google("Lexend Deca")),
-
+  theme = tema_intra,
+  
   useShinyjs(),
   
-  # CSS
-  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "apresent.css")),
-  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")),
-  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "qualidade.css")),
-  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "trimagem.css")),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "apresent.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "qualidade.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "trimagem.css")
+  ),
   
-  # Página de Navegação
   page_navbar(
-    # HEADER
-    title = tags$span(tags$img(src = "logo-principal-sf.png", id = "logo-fixo"),
-                      "INTRA",
-                      class = "titulo-app",
-                      style = "margin-right: 60px;"
+    
+    title = tags$span(
+      tags$img(
+        src = "logo-principal-sf.png",
+        id = "logo-fixo",
+        alt = "Logo da INTRA - Liga Acadêmica de Informática em Saúde"
+      ),
+      "INTRA",
+      class = "titulo-app"
     ),
     
-    # QUALIDADE 
-    nav_panel("Qualidade",
-              qualidadeUI("qualidade_id") 
-    ), 
+    nav_panel(
+      "Qualidade",
+      qualidadeUI("qualidade_id")
+    ),
     
-    # TRIMAGEM
-    nav_panel("Trimagem",
-              trimagemUI("trimagem_id")
-    ), 
+    nav_panel(
+      "Trimagem",
+      trimagemUI("trimagem_id")
+    ),
     
     nav_spacer(),
     
-    # Controlador de modo claro/escuro
-    nav_item(input_dark_mode(id = "mode", mode = "light")), 
+    nav_item(
+      input_dark_mode(id = "mode", mode = "light")
+    ),
     
     id = "page"
   )
